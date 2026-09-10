9ebe39f7927b58cdc00871351a8366058d480dc5
alice <jdj17180@gmail.com>
2026-09-10 03:19:07 +0200

    feat: utils/ - add shared configurations variables file

---
+++ utils/shared.py
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0
+#
+# vim: set ts=8 sw=8 noet tw=80 cc=80 fo+=t :
+
+PROJECT_NAME = "uranus"
+
+# ------| configuration directories
+CONFIG_DIR = "./configs/"
+# credentials
+CRED_CONFIG_DIR = CONFIG_DIR + "credentials/"
+BOOT_CONFIG_DIR = CONFIG_DIR + "boot/"
